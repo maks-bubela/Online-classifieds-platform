2fd491718e2291aa55cd5d17ef5fca1fa3bb782a
m_bubela <m_bubela@mifprojects.com>
2023-10-11 15:37:01 +0300

    Change project name to "OnlineClassifiedsPlatform"

--- OnlineClassifiedsPlatform/OnlineClassifiedsPlatform/Program.cs
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Online_classifieds_platform
+namespace OnlineClassifiedsPlatform
 {
     public class Program
     {
--- OnlineClassifiedsPlatform/OnlineClassifiedsPlatform/Startup.cs
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform/Startup.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Online_classifieds_platform
+namespace OnlineClassifiedsPlatform
 {
     public class Startup
     {
