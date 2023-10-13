7d816ccf8bdbc8e2fff6f6f1d90ad7712ecca6ed
m_bubela <m_bubela@mifprojects.com>
2023-10-13 18:24:23 +0300

    Added conficuration project

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.Configuration/OnlineClassifiedsPlatformConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace OnlineClassifiedsPlatform.Configuration
+{
+    public class OnlineClassifiedsPlatformConfiguration
+    {
+        public const string SqlConnectionStringSectionName = "ConnectionStrings:OnlineClassifiedsPlatformDB";
+
+
+        public string SqlConnectionString { get; private set; }
+
+
+        public OnlineClassifiedsPlatformConfiguration(string sqlConnectionString)
+        {
+            if (string.IsNullOrWhiteSpace(nameof(sqlConnectionString)))
+            {
+                throw new ArgumentException(nameof(sqlConnectionString));
+            }
+
+            SqlConnectionString = sqlConnectionString;
+        }
+
+        public static OnlineClassifiedsPlatformConfiguration CreateFromConfigurations()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configurationBuilder.AddJsonFile(path, false);
+
+            IConfigurationRoot root = configurationBuilder.Build();
+
+            return new OnlineClassifiedsPlatformConfiguration(
+                sqlConnectionString: root.GetSection(SqlConnectionStringSectionName).Value);
+        }
+    }
+}
