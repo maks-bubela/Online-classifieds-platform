031c706ad673f5d3a4303eea87b5c69baad8919c
m_bubela <m_bubela@mifprojects.com>
2023-10-13 18:25:52 +0300

    Added DTO for user

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.BLL/DTO/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace OnlineClassifiedsPlatform.BLL.DTO
+{
+    public class UserDTO
+    {
+        public long Id { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string RoleName { get; set; }
+    }
+}
