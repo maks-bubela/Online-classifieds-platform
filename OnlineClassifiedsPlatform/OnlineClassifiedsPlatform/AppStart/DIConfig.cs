cba247f568853a74e28a324c62606f3ca634c72b
m_bubela <m_bubela@mifprojects.com>
2023-10-13 18:28:44 +0300

    Added DIConfig

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform/AppStart/DIConfig.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using AutoMapper;
+using OnlineClassifiedsPlatform.Autofac.Modules;
+using OnlineClassifiedsPlatform.BLL.MappingProfiles;
+
+namespace OnlineClassifiedsPlatform.AppStart
+{
+    public class DIConfig
+    {
+        public static ContainerBuilder Configure(ContainerBuilder containerBuilder)
+        {
+
+            containerBuilder.RegisterModule<DALModule>();
+            containerBuilder.Register(ctx => new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new UserProfileBLL());
+            }).CreateMapper()).As<IMapper>().InstancePerLifetimeScope();
+            return containerBuilder;
+        }
+    }
+}
