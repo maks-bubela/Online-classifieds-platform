ecef510134cdf445ff1e3f64f4eea96a792974c7
m_bubela <m_bubela@mifprojects.com>
2023-10-13 18:19:22 +0300

    Added user entities

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.DAL/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using OnlineClassifiedsPlatform.DAL.Interfaces;
+
+namespace OnlineClassifiedsPlatform.DAL.Entities
+{
+    public class User : IEntity
+    {
+        public long Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Password { get; set; }
+
+        public long RoleId { get; set; }
+        public virtual Role Role { get; private set; }
+        public bool IsDelete { get; set; }
+
+        public string Salt { get; set; }
+
+    }
+}
