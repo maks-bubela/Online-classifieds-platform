2deffb85ef17913d42941e40cf0a6ce629e0fe41
m_bubela <m_bubela@mifprojects.com>
2023-10-13 18:23:43 +0300

    Added DAL modul for autofac

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.Autofac/Modules/DALModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using OnlineClassifiedsPlatform.Configuration;
+using OnlineClassifiedsPlatform.DAL;
+using OnlineClassifiedsPlatform.DAL.Interfaces;
+using OnlineClassifiedsPlatform.DAL.Repository;
+
+namespace OnlineClassifiedsPlatform.Autofac.Modules
+{
+    public class DALModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var connectionString = OnlineClassifiedsPlatformConfiguration.CreateFromConfigurations().SqlConnectionString;
+
+            builder.Register(ctx => new OnlineClassifiedsPlatformContext(connectionString)).AsSelf();
+            builder.RegisterType<GenericRepository>().As<IGenericRepository>();
+
+            base.Load(builder);
+        }
+    }
+}
