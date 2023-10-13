db6af863dfaf6eafb165e6a6e2ff8334a333fa98
m_bubela <m_bubela@mifprojects.com>
2023-10-13 18:29:15 +0300

    Added user info model

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform/Models/UserInfoModel.cs
@@ -0,0 +1,25 @@
+﻿namespace OnlineClassifiedsPlatform.Models
+{
+    public class UserInfoModel
+    {
+        /// <summary>
+        /// User username
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// User first name
+        /// </summary>
+        public string Firstname { get; set; }
+
+        /// <summary>
+        /// User last name
+        /// </summary>
+        public string Lastname { get; set; }
+
+        /// <summary>
+        /// User role
+        /// </summary>
+        public string Role { get; set; }
+    }
+}
