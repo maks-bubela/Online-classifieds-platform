7f9091b338cea10d20159c4aba10976840174f7a
m_bubela <m_bubela@mifprojects.com>
2023-10-13 18:27:38 +0300

    Added mapper profiles for user

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.BLL/MappingProfiles/UserProfileBLL.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using OnlineClassifiedsPlatform.BLL.DTO;
+using OnlineClassifiedsPlatform.DAL.Entities;
+
+namespace OnlineClassifiedsPlatform.BLL.MappingProfiles
+{
+    public class UserProfileBLL : Profile
+    {
+        public UserProfileBLL()
+        {
+            #region To DTO
+            CreateMap<User, UserDTO>();
+            #endregion
+
+            #region from DTO
+            CreateMap<UserDTO, User>();
+            #endregion
+        }
+    }
+}
