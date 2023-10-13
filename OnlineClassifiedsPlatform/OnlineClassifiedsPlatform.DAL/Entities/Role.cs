621743ce656e32cc5b985028ed07f6e35b73ee63
m_bubela <m_bubela@mifprojects.com>
2023-10-13 18:19:33 +0300

    Added role entities

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.DAL/Entities/Role.cs
@@ -0,0 +1,14 @@
+﻿using OnlineClassifiedsPlatform.DAL.Interfaces;
+using System.Collections.Generic;
+
+namespace OnlineClassifiedsPlatform.DAL.Entities
+{
+    public class Role : IEntity
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<User> Users { get; private set; } = new HashSet<User>();
+    }
+}
