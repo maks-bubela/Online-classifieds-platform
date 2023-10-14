d665897889ab42bb11eea5bec6f2c694d8264d2c
m_bubela <m_bubela@mifprojects.com>
2023-10-14 20:28:23 +0300

    Added Token service

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.BLL/Interfaces/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using OnlineClassifiedsPlatform.BLL.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineClassifiedsPlatform.BLL.Interfaces
+{
+    public interface ITokenService
+    {
+        Task<int> GetTokenSettingsAsync(EnvirementTypes type);
+    }
+}
---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.BLL/Services/TokenService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineClassifiedsPlatform.BLL.Enums;
+using OnlineClassifiedsPlatform.BLL.Exceptions;
+using OnlineClassifiedsPlatform.BLL.Interfaces;
+using OnlineClassifiedsPlatform.DAL.Context;
+using OnlineClassifiedsPlatform.DAL.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineClassifiedsPlatform.BLL.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly OnlineClassifiedsPlatformContext _ctx;
+        public TokenService(OnlineClassifiedsPlatformContext ctx)
+        {
+            _ctx = ctx ?? throw new ArgumentNullException(nameof(OnlineClassifiedsPlatformContext));
+        }
+        public async Task<int> GetTokenSettingsAsync(EnvirementTypes type)
+        {
+            var tokenSetting = await _ctx.Set<BearerTokenSetting>().Include(x => x.EnvironmentType)
+                .Where(b => b.EnvironmentType.Id == ((long)type))
+                .SingleOrDefaultAsync() ?? throw new EntityArgumentNullException(nameof(BearerTokenSetting));
+            return tokenSetting.LifeTime;
+        }
+    }
+}
