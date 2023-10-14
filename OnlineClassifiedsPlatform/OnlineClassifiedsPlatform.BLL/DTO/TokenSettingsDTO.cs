4de64cdc0b1500333cf72facf3cfb4d8891acc6e
m_bubela <m_bubela@mifprojects.com>
2023-10-14 20:28:39 +0300

    Added DTO for token settings

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.BLL/DTO/TokenSettingsDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace OnlineClassifiedsPlatform.BLL.DTO
+{
+    public class TokenSettingsDTO
+    {
+        public ClaimsIdentity Identity { get; set; }
+        public int LifeTime { get; set; }
+    }
+}
