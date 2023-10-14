b58fcfcf1f00807fd1f7016384f34c3dabfc1dba
m_bubela <m_bubela@mifprojects.com>
2023-10-14 20:26:08 +0300

    Added bearer token settings entity

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.DAL/Entities/BearerTokenSetting.cs
@@ -0,0 +1,14 @@
+﻿using OnlineClassifiedsPlatform.DAL.Interfaces;
+
+namespace OnlineClassifiedsPlatform.DAL.Entities
+{
+    public class BearerTokenSetting : IEntity
+    {
+        public long Id { get; set; }
+
+        public byte LifeTime { get; set; }
+
+        public long EnvironmentTypeId { get; set; }
+        public virtual EnvironmentType EnvironmentType { get; private set; }
+    }
+}
