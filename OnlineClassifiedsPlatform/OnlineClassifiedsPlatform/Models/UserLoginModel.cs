82a843367830b1c96039158a7e74463600f8c5a0
m_bubela <m_bubela@mifprojects.com>
2023-10-14 20:30:00 +0300

    Added models for user login and registration

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform/Models/UserLoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineClassifiedsPlatform.Models
+{
+    public class UserLoginModel
+    {
+        /// <summary>
+        /// User's username.
+        /// </summary>
+        [Required, MinLength(4), MaxLength(16)]
+        public string Username { get; set; }
+
+        /// <summary>
+        /// User's password.
+        /// </summary>
+        [Required, MinLength(6), MaxLength(16)]
+        public string Password { get; set; }
+    }
+}
---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform/Models/UserRegistrationModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineClassifiedsPlatform.Models
+{
+    public class UserRegistrationModel
+    {
+        /// <summary>
+        /// User username
+        /// </summary>
+        [Required, MinLength(4), MaxLength(16)]
+        public string Username { get; set; }
+        /// <summary>
+        /// User password
+        /// </summary>
+
+        [Required, MinLength(6), MaxLength(16)]
+        public string Password { get; set; }
+        /// <summary>
+        /// User first name
+        /// </summary>
+
+        [Required, MinLength(3), MaxLength(16)]
+        public string Firstname { get; set; }
+        /// <summary>
+        /// User last name
+        /// </summary>
+        [Required, MinLength(3), MaxLength(16)]
+        public string Lastname { get; set; }
+    }
+}
