3cd7a8e1024f72d24813232362a3065000617a3d
m_bubela <m_bubela@mifprojects.com>
2023-10-14 20:29:24 +0300

    Added module for services

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.Autofac/Modules/ServiceModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using OnlineClassifiedsPlatform.BLL.Cryptography;
+using OnlineClassifiedsPlatform.BLL.Interfaces;
+using OnlineClassifiedsPlatform.BLL.Services;
+
+
+namespace OnlineClassifiedsPlatform.Autofac.Modules
+{
+    public class ServiceModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            builder.RegisterType<AccountService>().As<IAccountService>();
+            builder.RegisterType<UserService>().As<IUserService>();
+            builder.RegisterType<TokenService>().As<ITokenService>();
+            builder.RegisterType<PasswordProcessing>().As<IPasswordProcessing>();
+            base.Load(builder);
+        }
+    }
+}
