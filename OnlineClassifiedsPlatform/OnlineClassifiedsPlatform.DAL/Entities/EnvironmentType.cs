d0f6fc10be784e15febaec9cd025b8da7b0fd664
m_bubela <m_bubela@mifprojects.com>
2023-10-14 20:25:41 +0300

    Added Envirement type to DB

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.DAL/Entities/EnvironmentType.cs
@@ -0,0 +1,15 @@
+﻿using OnlineClassifiedsPlatform.DAL.Interfaces;
+using System.Collections.Generic;
+
+namespace OnlineClassifiedsPlatform.DAL.Entities
+{
+    public class EnvironmentType : IEntity
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<BearerTokenSetting> BearerTokenSettings { get; private set; }
+            = new HashSet<BearerTokenSetting>();
+    }
+}
