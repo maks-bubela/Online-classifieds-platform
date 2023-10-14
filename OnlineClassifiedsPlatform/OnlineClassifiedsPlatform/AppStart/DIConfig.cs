d200eb1cf89f956367059535a7ab889fe60b39c2
m_bubela <m_bubela@mifprojects.com>
2023-10-14 20:31:32 +0300

    Added new conteiners

--- OnlineClassifiedsPlatform/OnlineClassifiedsPlatform/AppStart/DIConfig.cs
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform/AppStart/DIConfig.cs
@@ -2,6 +2,9 @@
 using AutoMapper;
 using OnlineClassifiedsPlatform.Autofac.Modules;
 using OnlineClassifiedsPlatform.BLL.MappingProfiles;
+using OnlineClassifiedsPlatform.Interfaces;
+using OnlineClassifiedsPlatform.JwtConfig.Provider;
+using OnlineClassifiedsPlatform.MappingProfiles;
 
 namespace OnlineClassifiedsPlatform.AppStart
 {
@@ -11,9 +14,12 @@ namespace OnlineClassifiedsPlatform.AppStart
         {
 
             containerBuilder.RegisterModule<DALModule>();
+            containerBuilder.RegisterModule<ServiceModule>();
+            containerBuilder.RegisterType<AuthOptions>().As<IAuthOptions>();
             containerBuilder.Register(ctx => new MapperConfiguration(cfg =>
             {
                 cfg.AddProfile(new UserProfileBLL());
+                cfg.AddProfile(new UserProfile());
             }).CreateMapper()).As<IMapper>().InstancePerLifetimeScope();
             return containerBuilder;
         }
