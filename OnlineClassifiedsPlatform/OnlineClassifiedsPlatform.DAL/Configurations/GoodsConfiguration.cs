9e2be84ea253ebf42825ac5a18af53a31ad61537
m_bubela <m_bubela@mifprojects.com>
2023-10-17 20:54:04 +0300

    Added new properties for Goods entity

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.DAL/Configurations/GoodsConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineClassifiedsPlatform.DAL.Entities;
+
+
+namespace OnlineClassifiedsPlatform.DAL.Configurations
+{
+    public class GoodsConfiguration : IEntityTypeConfiguration<Goods>
+    {
+        public void Configure(EntityTypeBuilder<Goods> builder)
+        {
+            builder.HasOne(u => u.GoodsCategory)
+                .WithMany(r => r.Goods)
+                .HasForeignKey(u => u.GoodsCategoryId).IsRequired();
+
+            builder.HasMany(u => u.GoodsPhotos)
+                .WithOne(r => r.Goods)
+                .HasForeignKey(u => u.GoodsImageId).IsRequired();
+
+            builder.HasOne(u => u.User)
+                .WithMany(r => r.Goods)
+                .HasForeignKey(u => u.UserId).IsRequired();
+        }
+    }
+}
--- OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.DAL/Entities/Goods.cs
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.DAL/Entities/Goods.cs
@@ -1,8 +1,21 @@
 ﻿
+using OnlineClassifiedsPlatform.DAL.Interfaces;
+using System.Collections.Generic;
+
 namespace OnlineClassifiedsPlatform.DAL.Entities
 {
-    public class Goods
+    public class Goods : IEntity
     {
         public long Id { get; set; }
+        public virtual GoodsCategory GoodsCategory { get; set; }
+        public long GoodsCategoryId { get; set; }
+        public string GoodsName { set; get; }
+        public string Description { get; set; }
+        public long Price { get; set; }
+        public string Condition { get; set; }
+        public virtual ICollection<GoodsPhoto> GoodsPhotos { get; private set; }
+            = new HashSet<GoodsPhoto>();
+        public virtual User User { get; set; }
+        public long UserId { get; set; }
     }
 }
