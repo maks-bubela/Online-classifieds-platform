d0abe1870f6d4ce7369462b84bb27c005e69dc59
m_bubela <m_bubela@mifprojects.com>
2023-10-17 20:56:35 +0300

    Added new entity ImageMetadata

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.DAL/Configurations/ImageMetadataConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineClassifiedsPlatform.DAL.Entities;
+
+namespace OnlineClassifiedsPlatform.DAL.Configurations
+{
+    public class ImageMetadataConfiguration : IEntityTypeConfiguration<ImageMetadata>
+    {
+        public void Configure(EntityTypeBuilder<ImageMetadata> builder)
+        {
+            builder.HasOne(x => x.Image)
+                .WithOne(x => x.ImageMetadata);
+        }
+    }
+}
---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.DAL/Entities/ImageMetadata.cs
@@ -0,0 +1,16 @@
+﻿using OnlineClassifiedsPlatform.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineClassifiedsPlatform.DAL.Entities
+{
+    public class ImageMetadata : IEntity
+    {
+        public long Id { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public long? ImageId { get; set; }
+        public virtual AzureBlobFile Image { get; set; }
+    }
+}
