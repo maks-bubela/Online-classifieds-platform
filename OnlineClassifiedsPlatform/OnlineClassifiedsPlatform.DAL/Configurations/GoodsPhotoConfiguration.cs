28c1161708dd1a4d4489336649aa24b5c4a62f2c
m_bubela <m_bubela@mifprojects.com>
2023-10-17 20:56:02 +0300

    Added new entity GoodsPhoto

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.DAL/Configurations/GoodsPhotoConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineClassifiedsPlatform.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineClassifiedsPlatform.DAL.Configurations
+{
+    class GoodsPhotoConfiguration : IEntityTypeConfiguration<GoodsPhoto>
+    {
+        public void Configure(EntityTypeBuilder<GoodsPhoto> builder)
+        {
+            builder.HasOne(x => x.Goods)
+                .WithMany(x => x.GoodsPhotos)
+                .HasForeignKey(x => x.GoodsImageId).IsRequired();
+
+            builder.HasOne(x => x.GoodsImage)
+                .WithOne(x => x.GoodsPhoto);
+        }
+    }
+}
---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.DAL/Entities/GoodsPhoto.cs
@@ -0,0 +1,13 @@
+﻿using OnlineClassifiedsPlatform.DAL.Interfaces;
+
+namespace OnlineClassifiedsPlatform.DAL.Entities
+{
+    public class GoodsPhoto : IEntity
+    {
+        public long Id { get; set; }
+        public virtual AzureBlobFile GoodsImage { get; set; }
+        public long? GoodsImageId { get; set; }
+        public virtual Goods Goods { get; set; }
+        public long GoodsId { get; set; }
+    }
+}
