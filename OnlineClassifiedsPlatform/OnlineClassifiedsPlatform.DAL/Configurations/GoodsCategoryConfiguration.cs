09ef53f4cd0c2b8c1fa6fe3f6b6d9696f9023177
m_bubela <m_bubela@mifprojects.com>
2023-10-17 20:55:42 +0300

    Added new entity GoodsCategory

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.DAL/Configurations/GoodsCategoryConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineClassifiedsPlatform.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineClassifiedsPlatform.DAL.Configurations
+{
+    class GoodsCategoryConfiguration : IEntityTypeConfiguration<GoodsCategory>
+    {
+        public void Configure(EntityTypeBuilder<GoodsCategory> builder)
+        {
+            builder.HasMany(r => r.Goods)
+                .WithOne(u => u.GoodsCategory)
+                .HasForeignKey(u => u.GoodsCategoryId);
+        }
+    }
+}
---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.DAL/Entities/GoodsCategory.cs
@@ -0,0 +1,12 @@
+﻿using OnlineClassifiedsPlatform.DAL.Interfaces;
+using System.Collections.Generic;
+
+namespace OnlineClassifiedsPlatform.DAL.Entities
+{
+    public class GoodsCategory : IEntity
+    {
+        public long Id { get; set; }
+        public string CategoryName { get; set; }
+        public virtual ICollection<Goods> Goods { get; private set; } = new HashSet<Goods>();
+    }
+}
