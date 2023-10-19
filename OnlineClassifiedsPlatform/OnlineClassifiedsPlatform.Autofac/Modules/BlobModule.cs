ec3cb400b78fd9222ba8880a7f2f3a36d8220333
m_bubela <m_bubela@mifprojects.com>
2023-10-19 15:34:35 +0300

    Added blob module to autofac

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.Autofac/Modules/BlobModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using OnlineClassifiedsPlatform.AzureStorage.BlobStorage;
+using OnlineClassifiedsPlatform.AzureStorage.Interfaces;
+using OnlineClassifiedsPlatform.Configuration;
+
+namespace OnlineClassifiedsPlatform.Autofac.Modules
+{
+    public class BlobModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var config = OnlineClassifiedsPlatformConfiguration.CreateFromConfigurations();
+            var connectionString = config.AzureStorageConnectionString;
+            builder.Register(prov => new BlobProvider(connectionString)).AsSelf().As<IBlobProvider>();
+            base.Load(builder);
+        }
+    }
+}
