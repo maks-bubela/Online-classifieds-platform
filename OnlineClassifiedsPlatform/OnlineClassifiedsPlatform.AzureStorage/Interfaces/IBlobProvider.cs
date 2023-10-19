23ec395fee9d78aedc63d7026aec7647514ac183
m_bubela <m_bubela@mifprojects.com>
2023-10-19 15:33:25 +0300

    Added interface for blob provider

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.AzureStorage/Interfaces/IBlobProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OnlineClassifiedsPlatform.AzureStorage.Interfaces
+{
+    public interface IBlobProvider
+    {
+        Task<Uri> UploadBlobAsync(BinaryData binaryData, string containerName, string fileName = null, string fileType = null);
+        Task<Uri> MoveAsync(string containerName, Uri sourceUri);
+        Task<byte[]> GetByteImageAsync(Uri sourceUri, string containerName);
+        Task<bool> DeleteFileAsync(string fileName, string containerName);
+        Task<bool> DeleteFileAsync(Uri sourceUri, string containerName);
+        Task<string> GetFileNameFromUri(Uri sourceUri);
+        Uri GetUri(string fileName, string containerName);
+    }
+}
