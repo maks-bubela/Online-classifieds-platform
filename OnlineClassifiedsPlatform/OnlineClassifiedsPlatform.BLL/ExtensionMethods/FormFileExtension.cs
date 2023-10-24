7d2a13b3e942b1a53166a8a10e28aa47036ddc2d
m_bubela <m_bubela@mifprojects.com>
2023-10-24 12:55:01 +0300

    Added extension for formFile

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform.BLL/ExtensionMethods/FormFileExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+
+namespace OnlineClassifiedsPlatform.BLL.ExtensionMethods
+{
+    public static class FormFileExtension
+    {
+        public static BinaryData ToBinaryData(this IFormFile postedFile)
+        {
+            using (var binaryReader = new BinaryReader(postedFile.OpenReadStream()))
+            {
+                byte[] fileData = binaryReader.ReadBytes((int)postedFile.Length);
+                var userFile = new BinaryData(fileData);
+                return userFile;
+            }
+        }
+    }
+}
