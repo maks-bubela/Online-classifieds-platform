47fdbb033209d86d65387236bf4c26205dfd7adc
m_bubela <m_bubela@mifprojects.com>
2023-10-29 20:31:33 +0300

    Added model to strip card

---
+++ OnlineClassifiedsPlatform/OnlineClassifiedsPlatform/Models/StripeCardModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineClassifiedsPlatform.Models
+{
+    public class StripeCardModel
+    {
+        /// <summary>
+        /// User's Id.
+        /// </summary>
+        [Required]
+        public long UserId { get; set; }
+
+        /// <summary>
+        /// Stripe card card number.
+        /// </summary>
+        [Required, MinLength(16), MaxLength(16)]
+        public int CardNumber { get; set; }
+
+        /// <summary>
+        /// Stripe card expiration year.
+        /// </summary>
+        [Required, MinLength(4), MaxLength(4)]
+        public int ExpirationYear { get; set; }
+
+        /// <summary>
+        /// Stripe card expiration month.
+        /// </summary>
+        [Required, MinLength(2), MaxLength(2)]
+        public int ExpirationMonth { get; set; }
+
+        /// <summary>
+        /// Stripe card cvc.
+        /// </summary>
+        [Required, MinLength(3), MaxLength(3)]
+        public int Cvc { get; set; }
+    }
+}
